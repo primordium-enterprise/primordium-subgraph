8c4fdd7f33fd31d034bf6d7e905ee704f2938ec0
BCJdevelopment <bcj@bcjdevelopment.com>
2024-02-23 02:41:36 -0700

    Account entity event handlers

--- src/primordium-token-v1.ts
+++ src/primordium-token-v1.ts
@@ -1,28 +1,50 @@
+import { Address, Bytes } from "@graphprotocol/graph-ts";
 import {
   DelegateChanged as DelegateChangedEvent,
   DelegateVotesChanged as DelegateVotesChangedEvent,
   SnapshotCreated as SnapshotCreatedEvent,
   Transfer as TransferEvent,
-} from "../generated/PrimordiumTokenV1/PrimordiumTokenV1"
-import {
-  Member,
-  Delegate,
-} from "../generated/schema"
+} from "../generated/PrimordiumTokenV1/PrimordiumTokenV1";
+import { Member, Delegate } from "../generated/schema";
+import { getGovernanceData, getOrCreateMember } from "./utils";
 
 export function handleDelegateChanged(event: DelegateChangedEvent): void {
-
+  let member = getOrCreateMember(event.params.delegator);
+  member.delegate =
+    event.params.toDelegate === Address.zero()
+      ? null
+      : changetype<Bytes>(event.params.toDelegate);
+  member.save();
 }
 
 export function handleDelegateVotesChanged(
-  event: DelegateVotesChangedEvent,
-): void {
+  event: DelegateVotesChangedEvent
+): void {}
 
-}
-
-export function handleSnapshotCreated(event: SnapshotCreatedEvent): void {
-
-}
+export function handleSnapshotCreated(event: SnapshotCreatedEvent): void {}
 
 export function handleTransfer(event: TransferEvent): void {
+  if (event.params.from === Address.zero()) {
+    const governanceData = getGovernanceData();
+    governanceData.totalSupply = governanceData.totalSupply.plus(
+      event.params.value
+    );
+    governanceData.save();
+  } else {
+    const fromMember = getOrCreateMember(event.params.from);
+    fromMember.tokenBalance = fromMember.tokenBalance.minus(event.params.value);
+    fromMember.save();
+  }
 
+  if (event.params.to === Address.zero()) {
+    const governanceData = getGovernanceData();
+    governanceData.totalSupply = governanceData.totalSupply.minus(
+      event.params.value
+    );
+    governanceData.save();
+  } else {
+    const toMember = getOrCreateMember(event.params.to);
+    toMember.tokenBalance = toMember.tokenBalance.plus(event.params.value);
+    toMember.save();
+  }
 }
